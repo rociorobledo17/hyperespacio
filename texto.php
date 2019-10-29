894676012c7eecf4d204df4b8579e3c6cbe6d39d
cristian412 <cristianamarillacloss@gmail.com>
2019-10-29 18:46:40 -0300

    texto

---
+++ texto.php
@@ -0,0 +1,54 @@
+<?php
+/*
+### MITAI 1
+ASOCIACIÓN DE PADRES PARA AYUDA Y SOSTÉN A PARAGUAY NIÑOS Y NIÑAS CON CÁNCER 
+El cáncer es una enfermedad que puede afectar a cualquier niño, y no respeta sexo, 
+clases sociales, raza o edad. En los niños los tumores son la segunda causa de muerte, 
+después de los accidentes. 
+Entre O y 18 años la leucemia es la causa más frecuente. Cada año 2000 niños mueren de cáncer, 
+sin embargo 2 de cada 3 niños pueden ser salvados si reciben a tiempo la atención adecuada. 
+Hasta hace pocos años, el cáncer infantil, era una enfermedad mortal en el 100% de los casos. 
+Actualmente a través de los progresos de la ciencia, se ha convertido en una enfermedad curable, 
+alcanzando porcentajes de curación alrededor del 80 % en los países del primer mundo. 
+Nosotros, como países tercer mundista, debemos luchar no sólo contra la enfermedad, sino 
+también contra la pobreza, la falta de drogas, las infecciones, el abandono al tratamiento, 
+la distancia de muchos niños que deben acudir hasta un centro especializado en la capital. 
+Debido a estas necesidades se fundó en el año 2002 la Asociación Mitaí. 
+Sus fundadores son padres de niños con cáncer y voluntarios que sintieron el llamado de 
+intervenir en el apoyo a la estructura sanitaria ya existente. La Asociación Mitaí se halla 
+inscripta al I.C.C.C.P.O. (Confederación Internacional de Organizaciones de padres de niños con cáncer.) 
+y es una asociación sin fines de lucro que necesita la ayuda y la colaboración de todos para mejorar 
+la calidad de vida del niño con cáncer y la de su familia. 
+#### MITAI 2
+La Asociación Mitaí inició su intervención en el 2003 en adecuar algunas salas para el tratamiento 
+de los niños en el Hospital Gral. Pediátrico Niños de Acosta Ñu. En el año 2005 se construyó dentro del 
+predio del Hospital una Casa Albergue con 10 habitaciones con baños, una sala comedor y sala para manualidades; 
+donde se hospedan los niños con uno de sus padres durante el tratamiento intensivo. Hemos reducido de esta 
+manera el abandono al tratamiento de un 40% a un 10%. 
+En el año 2008 el Ministerio de Salud Pública construye un nuevo servicio de Oncología 
+acondicionado por la Asociación Mitaí para la mejor estadía del paciente. 
+En el 2009 se inicia un nuevo proyecto "Espacio-Escuela" con el apoyo del Ministerio 
+de Educación para ayudar a los niños durante el periodo de internación, a no perder 
+la continuidad escolar". "El Hospital gasta más de 20.000 dólares por año por paciente, 
+en el tratamiento y sus complicaciones." El objetivo de la Asociación Mitaí es obtener 
+los fondos necesarios para brindarle al niño con cáncer un tratamiento adecuado, ya sea 
+con el mejoramiento de la infraestructura, con la adquisición de drogas o supliendo sus 
+múltiples necesidades. "De usted y su colaboración depende que los niños que padecen 
+esta grave enfermedad puedan ser curados; o ayudar a sobrellevar su tratamiento con una 
+mejor calidad de vida, y que tanto el niño como los padres no se MITAI sientan solos en 
+esta lucha." 
+Todos juntos podemos!! 
+PARAGUAY 
+ASOCIACIÓN DE PADRES PARA AYUDA Y SOSTÉN A NIÑOS Y NIÑAS CON CÁNCER 
+### MITAI 3
+Desea formar parte de este arúoiris? Contribuir es fácil!! 
+Graphic World Publicitaria - Tel. 202 445 
+Teléfonos: +595-21- 589 009 - int. 119 +595-971-176 161 
+E-mail: donaciones@asociacionmitai.com 
+O realizar sus aportes directamente a: 
+Asociación Mitaí Asociación para ayuda y sostén a 
+niños y niñas con cáncer BBVA- Cuenta Nro 40427/4. 
+Para más información visite nuestra Página Web: http://www.asociacionmitai.com 
+ASOCIACIÓN DE PADRES PARA AYUDA Y SOSTÉN A NIÑOS Y NIÑAS CON CÁNCER 
+*/
+?>
